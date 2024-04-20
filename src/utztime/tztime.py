e3c6ef651d2f115e5c8ca7feb3d0cb7760056b8c
Shane Powell <shaneapowell@hotmail.com>
2024-04-20 08:55:55 -0500

    Lint error in license header

--- src/utztime/tztime.py
+++ src/utztime/tztime.py
@@ -1,10 +1,10 @@
-# ---------------------------------------------------------------------#
-# Micropython Timezone Library                                         #
-#                                                                      #
-# Copyright (C) 2024 by Shane Powell
-# Copyright (C) 2018 by Jack Christensen                               #
-# licensed under GNU GPL v3.0, https://www.gnu.org/licenses/gpl.html   #
-#----------------------------------------------------------------------#
+# --------------------------------------------------------------------- #
+# Micropython Timezone Library                                          #
+#                                                                       #
+# Copyright (C) 2024 by Shane Powell                                    #
+# Copyright (C) 2018 by Jack Christensen                                #
+# licensed under GNU GPL v3.0, https://www.gnu.org/licenses/gpl.html    #
+# --------------------------------------------------------------------- #
 
 import sys
 import time
